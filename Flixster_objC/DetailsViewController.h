ad0dada2fc6aea266f84d262b8e1ac662ea90354
Makayla <makrodriguez@csumb.edu>
2022-05-16 10:42:01 -0700

    search bar

---
+++ Flixster_objC/DetailsViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailsViewController.h
+//  Flixster_objC
+//
+//  Created by Makayla Rodriguez on 5/15/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Flixster_objC/MoviesViewController.m
+++ Flixster_objC/MoviesViewController.m
@@ -8,12 +8,19 @@
 #import "MoviesViewController.h"
 #import "MovieCell.h"
 #import "UIImageView+AFNetworking.h"
+#import "DetailsViewController.h"
 
-@interface MoviesViewController () <UITableViewDataSource, UITableViewDelegate>
+@interface MoviesViewController () <UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate>
 
 @property (nonatomic, strong) NSArray *moviesArray;
-
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) UIRefreshControl *refreshControl;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (strong, nonatomic) NSArray *filteredData;
+
+
+//- (NSIndexPath *)indexPathForCell:(UITableViewCell *)cell;
 
 @end
 
@@ -24,11 +31,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.tableView.dataSource = self;
-    self.tableView.rowHeight = UITableViewAutomaticDimension;
+            
+    [self.activityIndicator startAnimating];
     
-    self.tableView.delegate = self;
-        
+    
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self; // numberOfRowsInSection, cellForRowAt
+    self.searchBar.delegate = self;
+    
+    
+    [self fetchMovies];
+    
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    [self.refreshControl addTarget:self action:@selector(fetchMovies) forControlEvents:UIControlEventValueChanged];
+    [self.tableView insertSubview:self.refreshControl atIndex:0];
+//    [self.tableView addSubview:self.refreshControl];
+    
+}
+
+- (void)fetchMovies {
+    
+    self.filteredData = self.moviesArray;
+
+    self.tableView.rowHeight = UITableViewAutomaticDimension;
+            
     NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=3434be17b7e2f47a93a211477a193c7e"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
@@ -43,25 +69,28 @@
                
                // TODO: Get the array of movies
                self.moviesArray = dataDictionary[@"results"];
+               self.filteredData = dataDictionary[@"results"];
                
                [self.tableView reloadData];
                // TODO: Store the movies in a property to use elsewhere
                // TODO: Reload your table view data
            }
+        [self.refreshControl endRefreshing];
+        [self.activityIndicator stopAnimating];
        }];
     [task resume];
     
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.moviesArray.count;
+    return self.filteredData.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
     MovieCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MovieCell"];
     
-    NSDictionary *movie = self.moviesArray[indexPath.row];
+    NSDictionary *movie = self.filteredData[indexPath.row];
     
     cell.titleLabel.text = movie[@"title"];
     cell.synopsisLabel.text = movie[@"overview"];
@@ -72,20 +101,47 @@
     NSURL *posterUrl = [NSURL URLWithString:fullPosterUrl];
     
     [cell.posterView setImageWithURL:posterUrl];
-    
+
     return cell;
     
 }
 
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+    
+    if (searchText.length != 0) {
+        NSPredicate *predicate = [NSPredicate predicateWithBlock:^BOOL(NSDictionary *evaluatedObject, NSDictionary *bindings) {
+            NSString *title = evaluatedObject[@"title"];
+            return [title containsString:searchText];
+        }];
+        self.filteredData = [self.moviesArray filteredArrayUsingPredicate:predicate];
 
-/*
-#pragma mark - Navigation
+        NSLog(@"%@", self.filteredData);
+    } else {
+        self.filteredData = self.moviesArray;
+    }
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    [self.tableView reloadData];
 }
-*/
+
+
+//#pragma mark - Navigation
+//
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+
+//- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+////     Get the new view controller using [segue destinationViewController].
+////     Pass the selected object to the new view controller.
+//
+//    NSDictionary *dataToPass = self.filteredData[myIndexPath.row];
+//    DetailsViewController *detailVC = [segue destinationViewController];
+//    detailVC.detailDict = dataToPass;
+//
+//}
+//
+//-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+//    [self performSegueWithIdentifier:@"DetailsViewController" sender:self];
+//}
+
 
 @end
